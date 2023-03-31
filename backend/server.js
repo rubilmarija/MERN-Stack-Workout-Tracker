97d7f5fa36029a0124041553004d19098420033e
Marija Rubil <rubilmarija@gmail.com>
2023-03-31 10:56:45 +0200

    Controller functions

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const workoutRoutes = require('./routes/workouts.js');
+const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
@@ -22,11 +22,11 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes);
 
 // conect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         // listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('listening on port 4000', process.env.PORT)
+            console.log('listening on port', process.env.PORT)
         })
     })
     .catch((error) => {
