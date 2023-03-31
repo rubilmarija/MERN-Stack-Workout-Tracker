8fd0359b4fe6259fd09246d195932c520cd2f750
Marija Rubil <rubilmarija@gmail.com>
2023-03-31 14:15:34 +0200

    Adding react context

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,7 +1,10 @@
 import { useState } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 
 
 const WorkoutForm = () => {
+    const { dispatch } = useWorkoutsContext()
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
@@ -31,9 +34,9 @@ const WorkoutForm = () => {
             setReps('')
             setError(null)
             console.log('New Workout added', json)
+            dispatch({ type: 'CREATE_WORKOUT', payload: json })
         }
     }
-
 
     return (
         <form className="create" onSubmit={handleSubmit}>
